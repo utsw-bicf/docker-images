441c94188e3fddea0c5fceeeb7dc28854fe6ded5
Jon Gesell <11574364+jgesell@users.noreply.github.com>
2020-09-23 16:43:35 -0500

    Added step to append latest image to relations.yaml, added test to print paths to relations.yaml[latest]

---
+++ scripts/getLatestPaths.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Prints out the paths for the unittest.yml files for all latest images to run their pytests
+"""
+
+import os
+import sys
+import re
+import yaml
+from check_pre-exist import load_yaml
+
+def main():
+    """
+    Main method
+    
+    """
+    if len(sys.argv) < 1:
+        print("Usage python3 scripts/getLatestPaths.py <relations.yaml path>")
+        sys.exit(1)
+    else:
+        relations = load_yaml(os.path.abspath(sys.argv[1]))
+        latest_images = relations['latest']
+        for image in latest_images:
+            print(image + "/" + latest_images[image] + "/unittest.yml")
