6482abaa8e24f27aafc6ae28c7bfa4fccbcfe639
Jon Gesell <11574364+jgesell@users.noreply.github.com>
2020-09-24 11:20:32 -0500

    Changed test name and moved to more appropriate location; changed calling for testing of images.

--- tests/ci_latest_paths.py
+++ tests/ci_latest_paths.py
@@ -7,7 +7,28 @@ import os
 import sys
 import re
 import yaml
-from check_pre_exist import load_yaml
+import imagecheck
+
+
+def load_yaml(master_yaml):
+    """
+    Loads a yaml file and returns a python yaml object
+    :param yaml_file: the yaml file to open and read
+    """
+    with open(master_yaml) as yaml_file:
+        yaml_data = yaml.safe_load(yaml_file)
+    yaml_file.close()
+    return yaml_data
+
+
+def pull_image(docker_image):
+    """
+    Ensures that the version of the image specified has been pulled
+    :param docker_image: str: Docker image to pull in the format '<organization>/<image_name>:<version>'
+    """
+    if os.system("docker pull " + docker_image) != 0:
+        print("ERROR: Unable to build " + docker_image)
+        sys.exit(1)
 
 
 def main():
@@ -23,17 +44,12 @@ def main():
         owner = sys.argv[1]
         relations = load_yaml(os.path.abspath(sys.argv[2]))
         latest_images = relations['latest']
-        test_path = os.getcwd()
-        print(test_path, file=sys.stderr)
         for image in latest_images:
             tag = latest_images[image]
             image_name = "{}/{}:{}".format(owner, image, tag).replace("+", "_")
+            pull_image(image_name)
             image_path = os.path.abspath(image + "/" + tag + "/unittest.yml")
-            if os.system("docker pull " + image_name) != 0:
-                print("ERROR: Unable to build " + image_name)
-                sys.exit(1)
-            if os.system("python3 tests/test_dockerfiles.py \"" + owner + "\" \"" + image_path + "\"") != 0:
-                print("ERROR: Image testing failed for " + image_name)
+            imagecheck.main(owner, image_path)
 
 
 if __name__ == "__main__":
