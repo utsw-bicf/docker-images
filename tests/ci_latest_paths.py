c1fd01708499ca6b9b17f89989a3f78125297938
Jon Gesell <11574364+jgesell@users.noreply.github.com>
2020-09-24 12:50:38 -0500

    Removed bad error statement from ci_latest_paths.py

--- tests/ci_latest_paths.py
+++ tests/ci_latest_paths.py
@@ -48,7 +48,6 @@ def main():
             image_name = "{}/{}:{}".format(owner, image, tag).replace("+", "_")
             pull_image(image_name)
             image_path = image + "/" + tag + "/unittest.yml"
-            print("Exit status: " + os.system("python3 tests/imagecheck.py \"" + owner + "\" " + image_path))
             if os.system("python3 tests/imagecheck.py \"" + owner + "\" " + image_path) != 0:
                 print("ERROR: Image testing failed for " + image_name)
                 sys.exit(1)
