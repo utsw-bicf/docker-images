561dfb877afc8edce360dde31679ba8f36faea4a
Jonathan Gesell <Jonathan.Gesell@UTSouthwestern.edu>
2020-09-24 11:49:05 -0500

    Removed absolute pathing.

--- tests/ci_latest_paths.py
+++ tests/ci_latest_paths.py
@@ -47,7 +47,7 @@ def main():
             tag = latest_images[image]
             image_name = "{}/{}:{}".format(owner, image, tag).replace("+", "_")
             pull_image(image_name)
-            image_path = os.path.abspath(image + "/" + tag + "/unittest.yml")
+            image_path = image + "/" + tag + "/unittest.yml"
             print("python3 tests/imagecheck.py \"" + owner + "\" " + image_path)
             if os.system("python3 tests/imagecheck.py \"" + owner + "\" " + image_path) != 0:
                 print("ERROR: Image testing failed for " + image_name)
